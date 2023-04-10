316d5632ee95c8359434c853c9632c39ec2433a8
TierraDeTinieblas <charagender@proton.me>
2023-04-10 17:34:54 +0100

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int i; // All lines end with a semicolon
+        // All lines must end with a semicolon
+
+        byte Mario = null; // bytes only hold a single byte of data. null is literally nothing
+        short notme = 32767; // shorts are two bytes in size
+        int i = 2147483647; // ints are 4 bytes in size
         float notpi = 3.14f; // Floats use are only 1 byte precise and must end with an f after the number
         double supernotpi = 3.14159f; // Doubles are 2 bytes precise and can end with either nothing, an f, or a d
-
+        boolean whoami = true; // Bools can only be either true and false. also lowercase
+        char letter = 'e'; // chars only store a single character
+        String name = "I am Ruby"; // Strings must be declared with a capital S and full quotation marks.
     }
 }
