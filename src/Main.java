87048ed9654b0d856f0664a9284b54879ee0c6cd
TierraDeTinieblas <tierradetinieblas@gmail.com>
2023-04-10 17:12:35 +0100

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int i; // All lines end with a semicolon
+        float notpi = 3.14f; // Floats use are only 1 byte precise and must end with an f after the number
+        double supernotpi = 3.14159f; // Doubles are 2 bytes precise and can end with either nothing, an f, or a d
+
     }
 }
