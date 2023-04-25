175cd63c6068551f88449686460ec3a471479ff9
chara <charagender@proton.me>
2023-04-25 17:01:46 +0000

    Finished the isGameOver method

---
+++ src/Exercise3/Exercise3b.java
@@ -0,0 +1,21 @@
+package Exercise3;
+
+public class Exercise3b {
+    public static void main(String[] args) {
+        /*
+         * Exercise 3:
+         * b) Bank Management System:
+         * Create a Bank Management System which has the following components:
+         * Person (which saves first, middle and last name, age and a "social security number")
+         * Bank (which saves a List of customers and their Checking Accounts, maximum overdraw amount)
+         * Checking Account (should have a current value, methods for taking and depositing money)
+         * Opening a Checking Account should only work via a Bank and must require a certain amount of initial deposit.
+         *
+         * BONUS: Make the Banking System Interactive (No Solution provided!)
+         *
+         *
+         */
+
+        System.out.println();
+    }
+}
